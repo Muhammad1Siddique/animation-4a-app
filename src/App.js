03b2270ee3d950c7ec1c7f10a38fca06d3a3d14f
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-19 07:42:13 +0500

    Basic data update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 
 function App() {
-  const {ref} = useWebAnimations({
+  const {ref, playState, getAnimation} = useWebAnimations({
     keyframes:[
       {transform: 'translate(0px)'},
       {transform: 'translate(500px)'}
     ],
     animationOptions: {
       duration: 3000,
-      iterations: Infinity,
+      iterations: 5,
       direction: "alternate",
       easing: "ease-in-out"
     }
   });
   return (
-    <div ref={ref} className="appStyle">
-     hello there
+    <div id="container">
+      <div>Animation state <strong>{playState}</strong></div>
+      <div ref={ref}>
+        <em>I Love Pakistan</em>
+      </div>
+      <button className="play" onClick={()=>{
+        getAnimation().play();
+      }}>Play</button>
+
+      <button className="pause" onClick={()=>{
+        getAnimation().pause();
+      }}>Pause</button>
     </div>
+    
   );
 }
 
